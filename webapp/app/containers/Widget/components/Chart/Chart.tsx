5df6e18acb12125eb671e0ed59c552e1d59373b6
xiaowei.song <songxiaowei@chinagoods.com>
2025-11-21 14:57:58 +0800

    debug(chart): add debug logs in renderChart method
    - Add console log for renderChart call
    - Log selectedChart value
    - Log props.metrics value

--- webapp/app/containers/Widget/components/Chart/Chart.tsx
+++ webapp/app/containers/Widget/components/Chart/Chart.tsx
@@ -28,6 +28,7 @@ export class Chart extends React.PureComponent<IChartProps, IChartStates> {
   }
 
   private renderChart = (props: IChartProps) => {
+    console.log('!!!! Chart.tsx renderChart called !!!!')
     const {
       selectedChart,
       renderType,
@@ -35,6 +36,8 @@ export class Chart extends React.PureComponent<IChartProps, IChartStates> {
       isDrilling,
       onError
     } = props
+    console.log('selectedChart:', selectedChart)
+    console.log('props.metrics:', props.metrics)
 
     if (renderType === 'loading') {
       return
